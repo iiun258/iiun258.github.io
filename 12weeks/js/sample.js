81c159c618e01f5ddd1a5f5cb4696fde4e866d20
iiun258 <48274719+iiun258@users.noreply.github.com>
2019-05-23 11:51:49 +0900

    Update sample.js

--- 12weeks/js/sample.js
+++ 12weeks/js/sample.js
@@ -1,6 +1,4 @@
-<script type="text/javascript">
-      function fnTest() {
-        document.write("자바스크립트환영!")
-        alert("자바스크립트환영!!");
-      }
-</script>
+  function fnTest() {
+    document.write("자바스크립트환영!")
+    alert("자바스크립트환영!!");
+  }
